f0656afc9fa75c459c87f9c0c509462d65a76e66
allenmylath <allengeorgemylath@gmail.com>
2026-04-26 13:39:34 +0530

    Update mod.rs

--- src/transport/mod.rs
+++ src/transport/mod.rs
@@ -2,10 +2,10 @@ pub mod base;
 pub mod input;
 pub mod output;
 pub mod params;
-pub mod websocket;                                          // ← add
+pub mod websocket;
 
 pub use base::BaseTransport;
 pub use input::BaseInputTransport;
-pub use output::BaseOutputTransport;
+pub use output::{BaseOutputTransport, OutputMessage};
 pub use params::TransportParams;
-pub use websocket::{WebSocketParams, WebSocketTransport};  // ← add
+pub use websocket::{WebSocketParams, WebSocketTransport};
