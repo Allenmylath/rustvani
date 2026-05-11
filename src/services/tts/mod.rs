ecf157ea6bbcfd346f56dd38c52280136f2e9a0d
Allenmylath <allengeorgemylath@gmail.com>
2026-05-11 11:11:23 +0530

    deepgram

--- src/services/tts/mod.rs
+++ src/services/tts/mod.rs
@@ -1,5 +1,7 @@
+pub mod deepgram;
 pub mod piper;
 pub mod sarvam;
 
+pub use deepgram::{DeepgramEncoding, DeepgramTtsConfig, DeepgramTtsHandler};
 pub use piper::{PiperModel, PiperQuality, PiperTtsConfig, PiperTtsHandler};
 pub use sarvam::{SarvamTtsConfig, SarvamTtsHandler, TtsModelConfig, get_model_config};
