4c7a181c6fb2d622234290ec99d5fd3c841a7b48
Allenmylath <allengeorgemylath@gmail.com>
2026-06-01 11:19:40 +0530

    added support for gnanitests pending

--- src/services/stt/mod.rs
+++ src/services/stt/mod.rs
@@ -1,6 +1,8 @@
+pub mod gnani;
 pub mod sarvam;
 pub mod sixtydb;
 
+pub use gnani::{GnaniSttConfig, GnaniSttHandler};
 pub use sarvam::{SarvamSttConfig, SarvamSttHandler};
 pub use sixtydb::{
     SixtyDbAudioEnhancement, SixtyDbContext, SixtyDbContextItem, SixtyDbEncoding,
